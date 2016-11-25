8b5fbc067675e9145ac84905c0512015ca849554
rgulle4 <rgulle4@lsu.edu>
2016-11-25 14:02:38 -0600

    Rm bluemix failed experiments

--- cm-get.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-curl --request GET \
-  --url 'https://api.ibm.com/mapinsights/eventservice/event/allevents?tenant_id=78d20ed9-572c-4e8b-a1fc-ce52dd3a5e39' \
-  --header 'accept: application/json' \
-  --header 'content-type: application/json'
-           #'&num_rec_in_page=10'\
-           #'$num_page=1'\
